3dd5c44d7f5ae6c1d79b677bbc574687c7526af3
G_Will <130619089+pretuwill@users.noreply.github.com>
2024-04-26 21:52:55 -0300

    Add files via upload
    Aula 26/04

---
+++ Dados/Cliente.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dados
+{
+    public class Cliente
+    {
+        public int Id { get; set; }
+        public TipoPessoa tipoPessoa { get; set; }
+        public Documento documento { get; set; }
+        public string razaoSocial { get; set; }
+        public DateTime dataNascimento { get; set; }
+        public string Nome { get; set; }
+        public string rua { get; set; }
+        public int numero { get; set; }
+        public string bairro { get; set; }
+        public string cidade { get; set; }
+        public string complemento { get; set; }
+        public string cep { get; set; }
+        public string telefone { get; set; }
+        public string Email { get; set; }
+        public string celular { get; set; }
+        public decimal limite { get; set; }
+    public override bool Equals(object obj)
+        {
+            return obj is Cliente cliente &&
+                   Id == cliente.Id &&
+                   tipoPessoa == cliente.tipoPessoa &&
+                   Nome == cliente.Nome &&
+                   Email == cliente.Email;
+        }
+
+        public override int GetHashCode()
+        {
+            int hashCode = -1601931259;
+            hashCode = hashCode * -1521134295 + Id.GetHashCode();
+            hashCode = hashCode * -1521134295 + tipoPessoa.GetHashCode();
+            hashCode = hashCode * -1521134295 + EqualityComparer<string>.Default.GetHashCode(Nome);
+            hashCode = hashCode * -1521134295 + EqualityComparer<string>.Default.GetHashCode(Email);
+            return hashCode;
+        }
+    }
+}
---
+++ Dados/Tipos.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dados
+{
+    public enum TipoPessoa { PESSOA_FISICA=1, PESSOA_JURIDICA=2};
+    public enum Documento { CPF = 1, CNPJ = 2 };
+}
